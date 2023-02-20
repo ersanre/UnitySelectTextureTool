1a5ade7d5e1e9cbf6a0bbe1ea95c75b29b89d754
Yao <ersanhd@gmail.com>
2023-02-20 15:45:39 +0800

    切换语言的判断

---
+++ Editor/PublicExtension/LanguageExtension.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public static class LanguageExtension
+{
+    public static bool IsChinese//是繁体中文或者简体中文返回true
+    {
+        get
+        {
+            if (IsChineseTW() || IsChineseSimple())
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+    //当前操作系统是否为简体中文
+    public static bool IsChineseSimple()
+    {
+        return System.Threading.Thread.CurrentThread.CurrentCulture.Name == "zh-CN";
+    }
+
+    //当前操作系统是否为繁体中文
+    public static bool IsChineseTW()
+    {
+        return System.Threading.Thread.CurrentThread.CurrentCulture.Name == "Zh-TW";
+    }
+
+
+}
